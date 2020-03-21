1eee8cd70186e937351835e330f08976b2fd4655
Satyam Mishra <satyamgusmishra@gmail.com>
2020-03-21 15:36:03 +0530

    Added feature of transliteration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from text_speech import mytts
+from my_trans import converthindi
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ def index():
 @app.route('/report', methods=['GET', 'POST'])
 def report():
 	global topic_name
+	global language_convert
 	topic_name = request.form['topic_name']
+	language_convert = converthindi(topic_name)
 	return render_template('page1.html')
 
 @app.route('/report_page_2', methods = ['GET', 'POST'])
@@ -23,6 +26,10 @@ def report_page_2():
 def text_to_speech():
 	return render_template('text_to_speech.html')
 
+
+@app.route('/english_to_hindi', methods = ['GET', 'POST'])
+def english_to_hindi():
+	return render_template('english_to_hindi.html', language_convert = language_convert)
 
 if __name__ == '__main__':
     app.run(debug = True)
