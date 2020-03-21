24bbc00a9e19c0a9f4eb2b9b437186205110c91f
Satyam Mishra <satyamgusmishra@gmail.com>
2020-03-21 20:21:38 +0530

    resolved the bug of ascii and utf-8 encoding

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 from flask import Flask, render_template, url_for, request
 from text_speech import mytts
 from my_trans import converthindi
+from prachi_scrap import prachi_tare # this gives link for iframe
+from scrap import star #this gives summary
 
 app = Flask(__name__)
 
@@ -12,7 +18,11 @@ def index():
 def report():
 	global topic_name
 	global language_convert
+	global topic_summary
+	global link_1
 	topic_name = request.form['topic_name']
+	topic_summary = star(topic_name)
+	link_1 = prachi_tare(topic_name)
 	language_convert = converthindi(topic_name)
 	return render_template('page1.html')
 
@@ -20,12 +30,12 @@ def report():
 def report_page_2():
 	language = request.form['language']
 	mytts(topic_name, language)
-	return render_template('done.html', language = language, topic_name = topic_name)
+	return render_template('done.html', language = language, topic_name = topic_name, topic_summary = topic_summary, 
+		link_1 = link_1)
 
 @app.route('/text_to_speech', methods = ['GET', 'POST'])
 def text_to_speech():
 	return render_template('text_to_speech.html')
-
 
 @app.route('/english_to_hindi', methods = ['GET', 'POST'])
 def english_to_hindi():
