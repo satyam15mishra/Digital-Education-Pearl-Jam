6326588fdd6b7e81f7d40bcc212353b0d02ef149
Satyam Mishra <satyamgusmishra@gmail.com>
2020-03-21 13:28:32 +0530

    Updated main.py and html templates
    fixed 404 errors and added text to speech functionality.
    finding a way to play it on screen (although the mp3 can be downloaded easily)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, url_for, request, session
-import os
-from trans import converthindi, convertenglish
-app.secret_key = os.urandom(16)
-
+from flask import Flask, render_template, url_for, request
+from text_speech import mytts
 
 app = Flask(__name__)
 
@@ -12,30 +9,21 @@ def index():
 
 @app.route('/report', methods=['GET', 'POST'])
 def report():
+	global topic_name
 	topic_name = request.form['topic_name']
-	return render_template('page1.html', topic_name = topic_name)
+	return render_template('page1.html')
 
-@app.route('/report_page_2')
+@app.route('/report_page_2', methods = ['GET', 'POST'])
 def report_page_2():
-	return render_template('page2.html')
+	language = request.form['language']
+	mytts(topic_name, language)
+	return render_template('done.html', language = language, topic_name = topic_name)
 
-@app.route("/", methods=['GET','POST'])
-def home():
-	return render_template('home.html')
-
-@app.route("/ans", methods=['GET','POST'])
-def index():
-	data = request.form['trans']
-	converthindi(data)
-	return (render_template('ans.html', answer=converthindi(data),data=data))
-
-@app.route("/hindi", methods=['GET','POST'])
-def hindi():
-	data = request.form['trans']
-	convertenglish(data)
-	return (render_template('hindi.html', answer=convertenglish(data), data=data))
+@app.route('/text_to_speech', methods = ['GET', 'POST'])
+def text_to_speech():
+	return render_template('text_to_speech.html')
 
 
 if __name__ == '__main__':
-    #app.run(host = '2409:4043:2203:d0ff:40a4:30d5:aa1e:9f32')
-    app.run(debug = True, host = '2409:4043:2e02:2a22:cdfe:cb12:4ce5:25bb')
+    app.run(debug = True)
+    #app.run(debug = True, host = '2409:4043:2e02:2a22:cdfe:cb12:4ce5:25bb')
