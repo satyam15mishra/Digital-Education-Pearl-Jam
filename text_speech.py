bacfdebcc68c51ca0cdef8c35aedcb94211b65d3
Satyam Mishra <satyamgusmishra@gmail.com>
2020-03-22 08:51:53 +0530

    small tweak
    to avoid last minute bug

--- text_speech.py
+++ text_speech.py
@@ -2,7 +2,6 @@ from gtts import gTTS
 
 def mytts(text, language = 'en-us'):
 	tts = gTTS(text, lang = language)
-	#tts_file_name = str(text)+'.mp3'
-	tts_file_name = 'song1.mp3'
+	tts_file_name = 'hacknitr.mp3'
 	path = 'static/'+tts_file_name
 	tts.save(path)
